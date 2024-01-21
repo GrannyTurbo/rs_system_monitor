da20eef6dfc521f72fbb9a87508503da480080a7
GrannyTurbo <iforidh@gmail.com>
2024-01-21 17:08:09 +0000

    d

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,15 @@ use sysinfo::{
     Components, Disks, Networks, System,
 };
 
-fn main() {
+fn main() -> Result<(), io::Error> {
+
+    //initialise tui
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
     let mut sys = System::new_all();
     sys.refresh_all();
-
-    println!("system:");
 
     // RAM and swap information:
     let memory: f64 = (sys.used_memory() as f64) / (sys.total_memory()) as f64 ;
@@ -18,12 +21,5 @@ fn main() {
     let swap: f64 = (sys.used_swap() as f64) / (sys.total_swap()) as f64 ;
     let swap = (swap * 100.0) as u64;
 
-    println!("  => memory: {memory}%");
-    println!("  => swap: {swap}%");
-
-    // Display system information:
-    println!("System name:             {:?}", System::name());
-    println!("System kernel version:   {:?}", System::kernel_version());
-    println!("System OS version:       {:?}", System::os_version());
-    println!("System host name:        {:?}", System::host_name());
+    Ok(())
 }
